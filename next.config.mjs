2c09da876eb7786033f98f97b00d2dc1a5331491
Jeff Sieu <jeffsy00@gmail.com>
2025-11-05 19:19:41 +0800

    feat: add github workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/constraint-solver" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
